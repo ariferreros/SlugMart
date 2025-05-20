12d3894b0860e2390a2598d1dbd3aab09dccad72
limslate <limslate@ucsc.edu>
2025-05-20 12:17:29 -0700

    Update GameScreen.js

--- game/src/GameScreen.js
+++ game/src/GameScreen.js
@@ -5,9 +5,9 @@ export async function loadGameScreen(app) {
   const scene = new Container()
   console.log('GAME SCREEN')
 
-  // load game sceen assets, maybe can liek use a bundle
+  // load game sceen assets, maybe can like use a bundle
   // hopefully can just reuse this screen for the different days
-  // and have like a conteinre on top for the food and characters
+  // and have like a counter on top for the food, day number, and characters
 
   return scene
 }
