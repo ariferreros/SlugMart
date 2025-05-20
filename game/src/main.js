21df144ffd0c6aa701be0079dd7f72ec8120eda7
Ari Ferreros <tinytabby66@gmail.com>
2025-05-19 21:02:57 -0700

    title

--- game/src/main.js
+++ game/src/main.js
@@ -1,35 +1,48 @@
-import { Application, Assets, Sprite } from "pixi.js";
-console.log('main running');
-(async () => {
+import { Application, Assets, Sprite, Text, TextStyle } from 'pixi.js'
+console.log('main running')
+;(async () => {
   // Create a new application
-  const app = new Application();
+  const app = new Application()
 
   // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
+  await app.init({ background: '#1099bb', resizeTo: window })
 
   // Append the application canvas to the document body
-  document.getElementById("pixi-container").appendChild(app.canvas);
+  document.getElementById('pixi-container').appendChild(app.canvas)
 
+  const headerStyle = new TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 24,
+    fontWeight: 'bold',
+    fill: 0xffffff,
+  })
+
+  const title = new Text({
+    text: 'SlugMart',
+    headerStyle,
+  })
+
+  app.stage.addChild(title)
   // Load the bunny texture
-  const texture = await Assets.load("./assets/bunny.png");
+  // const texture = await Assets.load("./assets/bunny.png");
 
-  // Create a bunny Sprite
-  const bunny = new Sprite(texture);
+  // // Create a bunny Sprite
+  // const bunny = new Sprite(texture);
 
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
+  // // Center the sprite's anchor point
+  // bunny.anchor.set(0.5);
 
-  // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
+  // // Move the sprite to the center of the screen
+  // bunny.position.set(app.screen.width / 2, app.screen.height / 2);
 
-  // Add the bunny to the stage
-  app.stage.addChild(bunny);
+  // // Add the bunny to the stage
+  // app.stage.addChild(bunny);
 
-  // Listen for animate update
-  app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
-  });
-})();
+  // // Listen for animate update
+  // app.ticker.add((time) => {
+  //   // Just for fun, let's rotate mr rabbit a little.
+  //   // * Delta is 1 if running at 100% performance *
+  //   // * Creates frame-independent transformation *
+  //   bunny.rotation += 0.1 * time.deltaTime;
+  // });
+})()
